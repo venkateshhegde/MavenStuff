eaccad4b4d2cb3cb6e22f950b677b98a54f6787d
ven <venkateshhegde@yahoo.com>
2019-08-02 21:33:56 -0400

    Converted to Multi Module POM! Hooray!

---
+++ MavenConstructs/maventest/model/src/test/java/crg/ven/test/TestThisToo.java
@@ -0,0 +1,15 @@
+package crg.ven.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class TestThisToo {
+
+    @Test
+    public void testMe()
+    {
+        Assertions.assertEquals(12, 12);
+
+    }
+}
